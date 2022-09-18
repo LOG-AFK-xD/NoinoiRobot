a2664d0f6e5b1cc448f2c999aef1a08d50cb63eb
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-09-18 23:54:15 +0530

    Update __main__.py

--- NoinoiRobot/__main__.py
+++ NoinoiRobot/__main__.py
@@ -577,7 +577,7 @@ def noi_about_callback(update, context):
             ),
         )
 
-elif query.data == "emiko_support":
+elif query.data == "noi_support":
         query.message.edit_text(
             text="*๏ Emiko support chats*"
             f"\nJoin My Support Group/Channel for see or report a problem on {dispatcher.bot.first_name}.",
