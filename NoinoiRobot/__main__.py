d26032a835fb5b99817fe3fd1e5193a232a245ca
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-09-19 09:12:37 +0530

    Update __main__.py

--- NoinoiRobot/__main__.py
+++ NoinoiRobot/__main__.py
@@ -577,25 +577,6 @@ def noi_about_callback(update, context):
             ),
         )
 
-elif query.data == "emiko_support":
-        query.message.edit_text(
-            text=f"*๏ Emiko support chats*"
-            f"\nJoin My Support Group/Channel for see or report a problem on {dispatcher.bot.first_name}.",
-            parse_mode=ParseMode.MARKDOWN,
-            reply_markup=InlineKeyboardMarkup(
-                [
-                 [
-                    InlineKeyboardButton(text="• Support", url="t.me/Team_shadow_support"),
-                    InlineKeyboardButton(text="Updates •", url="https://t.me/team_shadow_xD"),
-                 ],
-                 [
-                    InlineKeyboardButton(text="[► Back ◄]", callback_data="emiko_"),
-                 
-                 ]
-                ]
-            ),
-        )
-
 
     elif query.data == "emiko_credit":
         query.message.edit_text(
