562dd7fe207233619b2211fc8056778812609829
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-09-20 14:10:05 +0530

    Update math.py

--- NoinoiRobot/modules/math.py
+++ NoinoiRobot/modules/math.py
@@ -113,7 +113,7 @@ def log(update: Update, context: CallbackContext):
 
 
 
-__mod_name__ = "ᴍᴀᴛʜ"
+__mod_name__ = "Mᴀᴛʜ"
 
 SIMPLIFY_HANDLER = DisableAbleCommandHandler("math", simplify)
 FACTOR_HANDLER = DisableAbleCommandHandler("factor", factor)
