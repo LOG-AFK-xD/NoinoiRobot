63f102c96d57d364bd8621f3baf3f371716522c8
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-09-20 08:27:05 +0530

    Update alive.py

--- NoinoiRobot/modules/alive.py
+++ NoinoiRobot/modules/alive.py
@@ -7,27 +7,27 @@ from pyrogram import __version__ as pyrover
 from NoinoiRobot.events import register as MEMEK
 from NoinoiRobot import telethn as tbot
 
-PHOTO = "https://telegra.ph/file/113b9daeef77b928b9077.jpg"
+PHOTO = "https://telegra.ph/file/0afc9401213cab54fadca.jpg"
 
 @MEMEK(pattern=("/alive"))
 async def awake(event):
   tai = event.sender.first_name
-  LUNA = "**HELLO I AM NOINOI!** \n\n"
-  LUNA += "✨ **I'M WORKING PROPERLY** \n\n"
-  LUNA += "✨ **MY MASTER : [BAZIGAR](https://T.ME/BAZIGARYT)** \n\n"
+  LUNA = "**Hello I Am Azuka!** \n\n"
+  LUNA += "✨ **I'm Working Properly** \n\n"
+  LUNA += "✨ **My Master : [𐏓〬⃝ ⸙‌ٖٖٖٖٖٖٜٖٖٖٖٖٖ Official ➣LOG⛦ AFK xͮD ⸙‌ٖٖٖٖٖٖٜٖٖٖٖٖٖ ااـ꯭](https://T.ME/official_afk_xD)** \n\n"
   LUNA += f"✨ **Telethon Version : {tlhver}** \n\n"
-  LUNA += f"✨ **PYROGRAM VERSION : {pyrover}** \n\n"
-  LUNA += "**THANS FOR ADDING ME HEAR ❤️**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/NOINOI_BOT?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/NOINOISUPPORT")]]
+  LUNA += f"✨ **Pyrogram Version : {pyrover}** \n\n"
+  LUNA += "**Thans For Adding Me Hear.**"
+  BUTTON = [[Button.url("• Help", "https://t.me/azuka_robot?start=help"), Button.url("Creator •", "https://t.me/Piro_x_Power")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=LUNA,  buttons=BUTTON)
 
 @MEMEK(pattern=("/cmd"))
 async def reload(event):
   tai = event.sender.first_name
-  LUNA = "✅ **𝗛𝗘𝗔𝗥 𝗧𝗛𝗘 𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗙𝗢𝗥 𝗡𝗢𝗜𝗡𝗢𝗜 𝗠𝗨𝗦𝗜𝗖 & 𝗩𝗜𝗗𝗘𝗢 & 𝗘𝗫𝗧𝗔𝗥𝗡𝗔𝗟 𝗙𝗘𝗔𝗧𝗨𝗥𝗘𝗦 📌**"
+  LUNA = "✅ **Hear The Some Command For Azuka Music & Video & External Features 📌**"
   buttons = [
     [
-        InlineKeyboardButton(text="𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 📚", callback_data="luna_"),
+        InlineKeyboardButton(text="Command 📚", callback_data="luna_"),
     ],
             ]
   await tbot.send_file(event.chat_id, PHOTO, caption=LUNA,  buttons=BUTTON)
