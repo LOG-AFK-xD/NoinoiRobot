59a1d1a6fb6180cef14a058c445801e90952c60e
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-09-20 15:02:55 +0530

    Update shell.py

--- NoinoiRobot/modules/shell.py
+++ NoinoiRobot/modules/shell.py
@@ -45,6 +45,6 @@ def shell(update: Update, context: CallbackContext):
 
 SHELL_HANDLER = CommandHandler(["sh"], shell)
 dispatcher.add_handler(SHELL_HANDLER)
-__mod_name__ = "sʜᴇʟʟ"
+__mod_name__ = "Sʜᴇʟʟ"
 __command_list__ = ["sh"]
 __handlers__ = [SHELL_HANDLER]
